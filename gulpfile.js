6389e3d4fec4dd73a69e2d7651737829fe6858b2
ken.li <ken.li@corp.to8to.com>
2018-09-25 14:34:45 +0800

    添加JS和CSS版本控制

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-csso');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp')
+var pug = require('gulp-pug')
+var less = require('gulp-less')
+var minifyCSS = require('gulp-csso')
+var concat = require('gulp-concat')
+var sourcemaps = require('gulp-sourcemaps')
+var rev = require('gulp-rev')
+var revCollector = require('gulp-rev-collector')
+var rm = require('rimraf')
 
 gulp.task('html', function(){
   return gulp.src('src/templates/*.pug')
@@ -15,7 +18,10 @@ gulp.task('css', function(){
   return gulp.src('src/templates/*.less')
     .pipe(less())
     .pipe(minifyCSS())
+    .pipe(rev())
     .pipe(gulp.dest('dist/css'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/css'))
 });
 
 gulp.task('js', function(){
@@ -23,7 +29,29 @@ gulp.task('js', function(){
     .pipe(sourcemaps.init())
     .pipe(concat('app.min.js'))
     .pipe(sourcemaps.write())
+    .pipe(rev())
     .pipe(gulp.dest('dist/js'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/js'))
 });
 
-gulp.task('default', [ 'html', 'css', 'js' ]);
+gulp.task('rev', function () {
+  return gulp.src(['rev/**/*.json', 'dist/**/*.html'])
+    .pipe( revCollector({
+      replaceReved: true,
+      dirReplacements: {
+        'css': '../css',
+        'js': '../js',
+        'cdn/': function(manifest_value) {
+          return '//cdn' + (Math.floor(Math.random() * 9) + 1) + '.' + 'exsample.dot' + '/img/' + manifest_value
+        }
+      }
+    }))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean', function () {
+  rm.sync('@(dist|rev)')
+})
+
+gulp.task('default', [ 'html', 'css', 'js' ])
